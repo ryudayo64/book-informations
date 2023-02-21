7c82a4c947abd5d751208dc91cd7ff76b1e0a83c
FUJITARYUNOSUKE <116864638+FUJITARYUNOSUKE@users.noreply.github.com>
2023-02-21 09:53:19 +0900

    initial commit.
    練習

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html xmlns:v="urn:schemas-microsoft-com:vml"
+        xmlns:o="urn:schemas-microsoft-com:office:office"
+        xmlns:x="urn:schemas-microsoft-com:office:excel"
+        xmlns="http://www.w3.org/TR/REC-html40">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../style.css?<?=time()?>">
+    <link rel=File-List href="users.files/filelist.xml">
+    <link id="shLink" href="users.files/sheet001.htm">
+    <link id="shLink"> 
+    <meta name="Excel Workbook Frameset">
+    <meta http-equiv=Content-Type content="text/html; charset=utf-8">
+    <meta name=ProgId content=Excel.Sheet>
+    <meta name=Generator content="Microsoft Excel 15">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@900&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <![if !supportTabStrip]>
+    <!--↓↓↓QRコードライブラリ↓↓↓-->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CK9PC4CD47"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-28042024-1');
+        gtag('config', 'G-CK9PC4CD47');
+    </script>
+    <meta name="robots" content="none">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+    <meta name="theme-color" content="#000000">
+    <!--CSS-->
+    <!--
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/github-dark.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@900&display=swap" rel="stylesheet">
+    -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/highlight.min.js"></script>
+    <script>hljs.highlightAll();</script>
+</head>
+<body>
